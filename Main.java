a0f721f9edea3f2c2cf04a14393b90db62fca697
Tobias Jansing <tob.jansing@gmail.com>
2020-08-20 17:05:54 +0200

    Adding some basic ant task that runs a java file to create a test txt.

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.lang.Exception;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+public class Main {
+	public static void main (String[] args) {
+		SimpleDateFormat formatter = new SimpleDateFormat("ddMMyyyy_HHmmss");
+		String date = formatter.format(new Date());
+
+		try {
+			Files.write(Paths.get("testfile" + date + ".txt"), Arrays.asList("test"));
+			System.out.println("File created.");
+		} catch (Exception e) {
+			System.out.println("Error creating file!");
+		}
+	}
+}
